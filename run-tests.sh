1bfb57e984c5c50d8b16b3b491dee38ff0523e72
ref-humbold <rafkaleta@onet.pl>
2017-07-07 00:19:54 +0200

    Kolorowanie wypisywania

--- run-tests.sh
+++ run-tests.sh
@@ -5,11 +5,11 @@ PYUNIT_OUTPUT_DIR="pyunit_results"
 
 if test ! -d $TEST_DIR
 then
-    echo "Test directory not found."
+    echo "\033[0;31mTest directory not found.\033[0m"
     exit -1
 fi
 
-echo "Running Python tests:"
+echo "\033[1;34mRunning PyUnit tests:\033[0m"
 python3 -m xmlrunner discover $TEST_DIR
-echo "Moving XML reports to output directory..."
+echo "\033[1;34mMoving XML reports to output directory...\033[0m"
 rm -rf $PYUNIT_OUTPUT_DIR && mkdir $PYUNIT_OUTPUT_DIR && mv TEST-*.xml $PYUNIT_OUTPUT_DIR
