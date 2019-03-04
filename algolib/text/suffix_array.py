2a88114f193041fb716fd61252db45d2208c0f00
R. Kaleta <rafkaleta@onet.pl>
2019-03-04 21:23:44 +0100

    Suffix array creation

--- algolib/text/suffix_array.py
+++ algolib/text/suffix_array.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """SUFFIX ARRAY IMPLEMENTATION"""
 
+from queue import Queue
+
 
 class SuffixArray:
     def __init__(self, text):
@@ -74,3 +76,101 @@ class SuffixArray:
                 ln = 0 if ln == 0 else ln - 1
 
         return arr
+
+    def __create_array(t, k):
+        if len(t) < 2:
+            return [0]
+
+        n2 = (t.size() + 2) / 3
+        n1 = (t.size() + 1) / 3
+        n0 = t.size() / 3, n02 = n0 + n2
+        t12 = [i for i in range(0, len(t) + n2 - n1) if i % 3 != 0]
+
+        self.__sort_by_keys(t12, t, 2, k)
+        self.__sort_by_keys(t12, t, 1, k)
+        self.__sort_by_keys(t12, t, 0, k)
+
+        ix = 0
+        last0 = k
+        last1 = k
+        last2 = k
+        tn12 = [0] * n02
+
+        for i in t12:
+            if self.__get_elem(t, i) != last0 or self.__get_elem(t, i + 1) != last1 \
+                    or self.__get_elem(t, i + 2) != last2:
+                ix += 1
+                last0 = self.__get_elem(t, i)
+                last1 = self.__get_elem(t, i + 1)
+                last2 = self.__get_elem(t, i + 2)
+
+            if i % 3 == 1:
+                tn12[i / 3] = ix
+            else:
+                tn12[i / 3 + n2] = ix
+
+        if ix < n02:
+            sa12 = self.__create_array(tn12, ix + 1)
+
+            for i in range(0, len(sa12)):
+                tn12[sa12[i]] = i + 1
+        else:
+            sa12 = [0] * n02
+
+            for i in range(0, tn12):
+                sa12[tn12[i] - 1] = i
+
+        sa0 = [3 * i for i in sa12 if i < n2]
+        self.__sort_by_keys(sa0, t, 0, k)
+
+        return self.__merge(t, sa0, tn12, sa12)
+
+    def __merge(t0, sa0, t12, sa12):
+        sa = []
+        n2 = (t0.size() + 2) / 3
+        n1 = (t0.size() + 1) / 3
+        i0 = 0
+        i12 = n2 - n1
+
+        while i0 < len(sa0) and i12 < len(sa12):
+            pos12 = sa12[i12] * 3 + 1 if sa12[i12] < n2 else (sa12[i12] - n2) * 3 + 2
+            pos0 = sa0[i0]
+            cond = (self.__get_elem(t0, pos12), self.__get_elem(t12, sa12[i12] + n2)) \
+                <= (self.__get_elem(t0, pos0), self.__get_elem(t12, pos0 / 3)) \
+                if sa12[i12] < n2 else \
+                (self.__get_elem(t0, pos12), self.__get_elem(t0, pos12 + 1),
+                 self.__get_elem(t12, sa12[i12] - n2 + 1)) \
+                <= (self.__get_elem(t0, pos0), self.__get_elem(t0, pos0 + 1),
+                    self.__get_elem(t12, pos0 / 3 + n2))
+
+            if cond:
+                sa.append(pos12)
+                i12 += 1
+            else:
+                sa.append(pos0)
+                i0 += 1
+
+        while i12 < len(sa12):
+            sa.append(sa12[i12] * 3 + 1 if sa12[i12] < n2 else (sa12[i12] - n2) * 3 + 2)
+            i12 += 1
+
+        while i0 < len(sa0):
+            sa.append(sa0[i0])
+            i0 += 1
+
+        return sa
+
+    def __sort_by_keys(v, keys, shift, k):
+        buckets = [Queue() for i in range(k)]
+        j = 0
+
+        for i in v:
+            buckets[self.__get_elem(keys, i + shift)].put(i)
+
+        for e in buckets:
+            while not e.empty():
+                v[j] = e.get()
+                j += 1
+
+    def __get_elem(v, i):
+        return v[i] if i < len(v) else 0
