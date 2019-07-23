05e62d1c691b2103f811792fda6b56993183b564
R. Kaleta <rafkaleta@onet.pl>
2019-07-23 13:43:27 +0200

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,20 +1,15 @@
 #! /bin/sh
 
 TEST_DIR="tests"
-BOLD_BLUE="\033[1;34m"
-BOLD_RED="\033[1;31m"
-NORMAL="\033[0m"
+NORMAL="\033[00m"
+BOLD_BLUE="\033[01;34m"
+BOLD_RED="\033[01;31m"
 
 cd $(dirname $0)
 echo ""
 
-if test ! -d $TEST_DIR
-then
-    echo "${BOLD_RED}Test directory not found.${NORMAL}"
-    exit 1
-fi
+[ ! -d $TEST_DIR ] && echo -e "${BOLD_RED}Test directory not found.${NORMAL}" && exit 1
 
-echo "${BOLD_BLUE}Running PyUnit tests with Nose2:${NORMAL}"
+echo -e "${BOLD_BLUE}Running PyUnit tests with Nose2:${NORMAL}"
 nose2 $TEST_DIR
-echo "${BOLD_BLUE}Generating XML report...${NORMAL}"
-echo ""
+echo -e"${BOLD_BLUE}Generating XML report...${NORMAL}\n"
