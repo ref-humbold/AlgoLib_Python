d936415f909584688dc0e2ef7f83685b14e6315a
R. Kaleta <rafkaleta@onet.pl>
2020-04-22 00:48:19 +0200

    Renaming and tests for multiple pop calls

--- algolib/structures/double_heap.py
+++ algolib/structures/double_heap.py
@@ -18,7 +18,7 @@ class DoubleHeap:
         return len(self._heap)
 
     @property
-    def front(self):
+    def left(self):
         """Retrieves minimal element from this double heap.
 
         :return: minimal element
@@ -29,7 +29,7 @@ class DoubleHeap:
         return self._heap[self._INDEX_FRONT]
 
     @property
-    def back(self):
+    def right(self):
         """Retrieves maximal element from this double heap.
 
         :return: maximal element
@@ -55,57 +55,57 @@ class DoubleHeap:
         if index % 2 == 1:
             if self._key(self._heap[index]) < self._key(self._heap[index - 1]):
                 self._heap[index - 1], self._heap[index] = self._heap[index], self._heap[index - 1]
-                self._move_to_front(index - 1)
+                self._move_to_left(index - 1)
             else:
-                self._move_to_back(index)
+                self._move_to_right(index)
         else:
             new_index = ((index + 1) // 2 - 1) // 2 * 2 + 1
 
             if self._key(self._heap[index]) > self._key(self._heap[new_index]):
                 self._heap[new_index], self._heap[index] = self._heap[index], self._heap[new_index]
-                self._move_to_back(new_index)
+                self._move_to_right(new_index)
             else:
-                self._move_to_front(index)
+                self._move_to_left(index)
 
-    def pop_front(self):
+    def popleft(self):
         """Retrieves and removes minimal element from this double heap.
 
         :return: removed minimal element
         :raise KeyError: if double heap is empty
         """
-        minimal = self.front
+        minimal = self.left
 
         self._heap[self._INDEX_FRONT] = self._heap[-1]
         del self._heap[-1]
-        self._move_to_back(self._INDEX_FRONT)
+        self._move_to_right(self._INDEX_FRONT)
 
         return minimal
 
-    def pop_back(self):
+    def popright(self):
         """Retrieves and removes maximal element from this double heap.
 
         :return: removed maximal element
         :raise KeyError: if double heap is empty
         """
         if len(self) == 1:
-            return self.pop_front()
+            return self.popleft()
 
-        maximal = self.back
+        maximal = self.right
         self._heap[self._INDEX_BACK] = self._heap[-1]
         del self._heap[-1]
-        self._move_to_front(self._INDEX_BACK)
+        self._move_to_left(self._INDEX_BACK)
 
         return maximal
 
     def clear(self):
         self._heap = []
 
-    def _move_to_front(self, index):
+    def _move_to_left(self, index):
         if index == self._INDEX_FRONT:
             return
 
         if index % 2 == 0:
-            self._step_to_front(index, (index // 2 - 1) // 2 * 2)
+            self._step_to_left(index, (index // 2 - 1) // 2 * 2)
         else:
             left_index = index + index + 1
             right_index = index + index + 3
@@ -115,23 +115,23 @@ class DoubleHeap:
                     self._key(self._heap[left_index]) > self._key(self._heap[right_index]) else \
                     right_index
 
-                self._step_to_front(index, child_index)
+                self._step_to_left(index, child_index)
             elif left_index < len(self):
-                self._step_to_front(index, left_index)
-            else:
-                self._step_to_front(index, index - 1)
+                self._step_to_left(index, left_index)
+            elif index < len(self):
+                self._step_to_left(index, index - 1)
 
-    def _step_to_front(self, index, next_index):
+    def _step_to_left(self, index, next_index):
         if self._key(self._heap[index]) < self._key(self._heap[next_index]):
             self._heap[index], self._heap[next_index] = self._heap[next_index], self._heap[index]
-            self._move_to_front(next_index)
+            self._move_to_left(next_index)
 
-    def _move_to_back(self, index):
+    def _move_to_right(self, index):
         if index == self._INDEX_BACK:
             return
 
         if index % 2 == 1:
-            self._step_to_back(index, (index // 2 - 1) // 2 * 2 + 1)
+            self._step_to_right(index, (index // 2 - 1) // 2 * 2 + 1)
         else:
             left_index = index + index + 2
             right_index = index + index + 4
@@ -141,13 +141,13 @@ class DoubleHeap:
                     self._key(self._heap[left_index]) < self._key(self._heap[right_index]) else \
                     right_index
 
-                self._step_to_back(index, child_index)
+                self._step_to_right(index, child_index)
             elif left_index < len(self):
-                self._step_to_back(index, left_index)
+                self._step_to_right(index, left_index)
             elif index + 1 < len(self):
-                self._step_to_back(index, index + 1)
+                self._step_to_right(index, index + 1)
 
-    def _step_to_back(self, index, next_index):
+    def _step_to_right(self, index, next_index):
         if self._key(self._heap[index]) > self._key(self._heap[next_index]):
             self._heap[index], self._heap[next_index] = self._heap[next_index], self._heap[index]
-            self._move_to_back(next_index)
+            self._move_to_right(next_index)
--- tests/test_double_heap.py
+++ tests/test_double_heap.py
@@ -30,40 +30,40 @@ class DoubleHeapTest(unittest.TestCase):
         # then
         self.assertEqual(len(self._numbers), result)
 
-    def test_front__when_empty__then_key_error(self):
+    def test_left__when_empty__then_key_error(self):
         # given
         self._test_object = DoubleHeap()
         # then
         with self.assertRaises(KeyError):
             # when
-            _ = self._test_object.front
+            _ = self._test_object.left
 
-    def test_front__when_not_empty__then_minimal_element(self):
+    def test_left__when_not_empty__then_minimal_element(self):
         # when
-        result = self._test_object.front
+        result = self._test_object.left
         # then
         self.assertEqual(min(self._numbers), result)
 
-    def test_back__when_empty__then_key_error(self):
+    def test_right__when_empty__then_key_error(self):
         # given
         self._test_object = DoubleHeap()
         # then
         with self.assertRaises(KeyError):
             # when
-            _ = self._test_object.back
+            _ = self._test_object.right
 
-    def test_back__when_single_element__then_this_element(self):
+    def test_right__when_single_element__then_this_element(self):
         # given
         element = 19
         self._test_object = DoubleHeap([element])
         # when
-        result = self._test_object.back
+        result = self._test_object.right
         # then
         self.assertEqual(element, result)
 
-    def test_back__when_multiple_elements__then_maximal_element(self):
+    def test_right__when_multiple_elements__then_maximal_element(self):
         # when
-        result = self._test_object.back
+        result = self._test_object.right
         # then
         self.assertEqual(max(self._numbers), result)
 
@@ -75,84 +75,104 @@ class DoubleHeapTest(unittest.TestCase):
         self._test_object.push(element)
         # then
         self.assertEqual(1, len(self._test_object))
-        self.assertEqual(element, self._test_object.front)
-        self.assertEqual(element, self._test_object.back)
+        self.assertEqual(element, self._test_object.left)
+        self.assertEqual(element, self._test_object.right)
 
-    def test_push__when_new_element_is_less__then_added_to_front(self):
+    def test_push__when_new_element_is_less__then_added_to_left(self):
         # given
         element = min(self._numbers) - 1
         # when
         self._test_object.push(element)
         # then
         self.assertEqual(len(self._numbers) + 1, len(self._test_object))
-        self.assertEqual(element, self._test_object.front)
+        self.assertEqual(element, self._test_object.left)
 
-    def test_push__when_new_element_is_greater__then_added_to_back(self):
+    def test_push__when_new_element_is_greater__then_added_to_right(self):
         # given
         element = max(self._numbers) + 1
         # when
         self._test_object.push(element)
         # then
         self.assertEqual(len(self._numbers) + 1, len(self._test_object))
-        self.assertEqual(element, self._test_object.back)
+        self.assertEqual(element, self._test_object.right)
 
     def test_push__when_new_element__then_added(self):
         # when
         self._test_object.push(46)
         # then
         self.assertEqual(len(self._numbers) + 1, len(self._test_object))
-        self.assertEqual(min(self._numbers), self._test_object.front)
-        self.assertEqual(max(self._numbers), self._test_object.back)
+        self.assertEqual(min(self._numbers), self._test_object.left)
+        self.assertEqual(max(self._numbers), self._test_object.right)
 
-    def test_pop_front__when_empty__then_key_error(self):
+    def test_popleft__when_empty__then_key_error(self):
         # given
         self._test_object = DoubleHeap()
         # then
         with self.assertRaises(KeyError):
             # when
-            _ = self._test_object.pop_front()
+            _ = self._test_object.popleft()
 
-    def test_pop_front__when_not_empty__then_minimal_element_removed(self):
+    def test_popleft__when_not_empty__then_minimal_element_removed(self):
         # when
-        result = self._test_object.pop_front()
+        result = self._test_object.popleft()
         # then
         self.assertEqual(len(self._numbers) - 1, len(self._test_object))
         self.assertEqual(min(self._numbers), result)
 
-    def test_pop_front__when_single_element__then_this_element_removed(self):
+    def test_popleft__when_single_element__then_this_element_removed(self):
         # given
         element = 19
         self._test_object = DoubleHeap([element])
         # when
-        result = self._test_object.pop_front()
+        result = self._test_object.popleft()
         # then
         self.assertEqual(0, len(self._test_object))
         self.assertEqual(element, result)
 
-    def test_pop_back__when_empty__then_key_error(self):
+    def test_popright__when_empty__then_key_error(self):
         # given
         self._test_object = DoubleHeap()
         # then
         with self.assertRaises(KeyError):
             # when
-            _ = self._test_object.pop_back()
+            _ = self._test_object.popright()
 
-    def test_pop_back__when_single_element__then_this_element_removed(self):
+    def test_popleft__when_multiple_calls__then_sorted_ascending(self):
+        # when
+        result = []
+
+        while len(self._test_object) > 0:
+            result.append(self._test_object.popleft())
+
+        # then
+        self.assertListEqual(sorted(self._numbers), result)
+
+    def test_popright__when_single_element__then_this_element_removed(self):
         # given
         element = 19
         self._test_object = DoubleHeap([element])
         # when
-        result = self._test_object.pop_back()
+        result = self._test_object.popright()
         # then
         self.assertEqual(0, len(self._test_object))
         self.assertEqual(element, result)
 
-    def test_pop_back__when_multiple_elements__then_maximal_element_removed(self):
+    def test_popright__when_multiple_elements__then_maximal_element_removed(self):
         # when
-        result = self._test_object.pop_back()
+        result = self._test_object.popright()
         # then
         self.assertEqual(len(self._numbers) - 1, len(self._test_object))
         self.assertEqual(max(self._numbers), result)
+
+    def test_popright__when_multiple_calls__then_sorted_descending(self):
+        # when
+        result = []
+
+        while len(self._test_object) > 0:
+            result.append(self._test_object.popright())
+
+        # then
+        self.assertListEqual(sorted(self._numbers, key=lambda x: -x), result)
 
     def test_clear__when_not_empty__then_empty(self):
         # when
