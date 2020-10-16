4c4334debc5c678bc095f0c6bccd500197d1bb81
R. Kaleta <rafkaleta@onet.pl>
2020-10-16 23:34:10 +0200

    Fix imports

--- algolib/text/__init__.py
+++ algolib/text/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .kmp import kmp
-from .kmr import kmr
+from .kmr import BaseWordsDict
 from .suffix_array import SuffixArray
 
-__all__ = ["kmp", "kmr", "SuffixArray"]
+__all__ = ["kmp", "BaseWordsDict", "SuffixArray"]
