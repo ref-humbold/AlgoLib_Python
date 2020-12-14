8852aa84ce633bb1b9f70292aa325a73f522c2c2
R. Kaleta <rafkaleta@onet.pl>
2020-12-14 21:36:19 +0100

    Rename module docstring

--- algolib/text/kmr.py
+++ algolib/text/kmr.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Base words dictionary structure using Karp-Miller-Rosenberg algorithm"""
+"""Structure of base words dictionary using Karp-Miller-Rosenberg algorithm"""
 
 
 class BaseWordsDict:
