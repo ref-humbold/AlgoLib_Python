0c38d3d44ee5d2c2269042c358ed26a2b1ce1beb
R. Kaleta <rafkaleta@onet.pl>
2021-04-04 00:50:00 +0200

    Stubs for arithmetic operators

--- algolib/mathmat/fraction.py
+++ algolib/mathmat/fraction.py
@@ -18,14 +18,14 @@ class Fraction:
     def __str__(self):
         return f"{self._numerator}/{self._denominator}"
 
-    def __float__(self):
-        return self._numerator / self._denominator
+    def __bool__(self):
+        return self._numerator != 0
 
     def __int__(self):
         return self._numerator // self._denominator
 
-    def __bool__(self):
-        return self._numerator != 0
+    def __float__(self):
+        return self._numerator / self._denominator
 
     def __eq__(self, frac: "Fraction"):
         return self._numerator == frac._numerator and self._denominator == frac._denominator
@@ -33,17 +33,50 @@ class Fraction:
     def __ne__(self, frac: "Fraction"):
         return not self == frac
 
-    def __neg__(self):
+    def __add__(self, frac: "Fraction") -> "Fraction":
+        pass
+
+    def __iadd__(self, frac: "Fraction"):
+        pass
+
+    def __sub__(self, frac: "Fraction") -> "Fraction":
+        pass
+
+    def __isub__(self, frac: "Fraction"):
+        pass
+
+    def __mul__(self, frac: "Fraction") -> "Fraction":
+        return Fraction(self._numerator * frac._numerator, self._denominator * frac._denominator)
+
+    def __imul__(self, frac: "Fraction"):
+        self._numerator *= frac._numerator
+        self._denominator *= frac._denominator
+        self._normalize()
+        return self
+
+    def __truediv__(self, frac: "Fraction") -> "Fraction":
+        if frac._numerator == 0:
+            raise ZeroDivisionError()
+
+        return Fraction(self._numerator * frac._denominator, self._denominator * frac._numerator)
+
+    def __itruediv__(self, frac: "Fraction"):
+        self._numerator *= frac._denominator
+        self._denominator *= frac._numerator
+        self._normalize()
+        return self
+
+    def __neg__(self) -> "Fraction":
         return Fraction(-self._numerator, self._denominator)
 
-    def __invert__(self):
+    def __abs__(self) -> "Fraction":
+        return Fraction(abs(self._numerator), self._denominator)
+
+    def __invert__(self) -> "Fraction":
         if self._numerator == 0:
             raise ArithmeticError("Inverting zero")
 
         return Fraction(self._denominator, self._numerator)
-
-    def __abs__(self):
-        return Fraction(abs(self._numerator), self._denominator)
 
     def _normalize(self):
         if self._denominator < 0:
