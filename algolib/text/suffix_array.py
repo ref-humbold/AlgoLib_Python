149c4fb06d1937304e974d9b6a34ddc4f5228dbb
R. Kaleta <rafkaleta@onet.pl>
2021-06-04 23:47:23 +0200

    Refactor suffix array

--- algolib/text/suffix_array.py
+++ algolib/text/suffix_array.py
@@ -92,45 +92,49 @@ class SuffixArray:
 
         return arr
 
-    def _create_array(self, text):
+    def _create_array(self, txt):
         # Creates a suffix array assuming a sized alphabet.
-        if len(text) < 2:
+        if len(txt) < 2:
             return [0]
 
-        lengths = (len(text) // 3, (len(text) + 1) // 3, (len(text) + 2) // 3)
-        lengths02 = lengths[0] + lengths[2]
-        text12 = [i for i in range(0, len(text) + lengths[2] - lengths[1]) if i % 3 != 0]
-        SuffixArray._sort_by_keys(text12, text, 2)
-        SuffixArray._sort_by_keys(text12, text, 1)
-        SuffixArray._sort_by_keys(text12, text, 0)
-        index = 0
-        last = (inf, inf, inf)
-        text_new12 = [0] * lengths02
+        lengths = (len(txt) // 3, (len(txt) + 1) // 3, (len(txt) + 2) // 3)
+        length02 = lengths[0] + lengths[2]
+        indices12 = [i for i in range(0, len(txt) + lengths[2] - lengths[1]) if i % 3 != 0]
 
-        for i in text12:
-            if last != (self._get(text, i), self._get(text, i + 1), self._get(text, i + 2)):
-                index += 1
-                last = (self._get(text, i), self._get(text, i + 1), self._get(text, i + 2))
+        SuffixArray._sort_indices(indices12, txt, 2)
+        SuffixArray._sort_indices(indices12, txt, 1)
+        SuffixArray._sort_indices(indices12, txt, 0)
+
+        code = 0
+        last = (inf, inf, inf)
+        text12 = [0] * length02
+
+        for i in indices12:
+            elems = (self._get(txt, i), self._get(txt, i + 1), self._get(txt, i + 2))
+
+            if last != elems:
+                code += 1
+                last = elems
 
             if i % 3 == 1:
-                text_new12[i // 3] = index
+                text12[i // 3] = code
             else:
-                text_new12[i // 3 + lengths[2]] = index
+                text12[i // 3 + lengths[2]] = code
 
-        if index < lengths02:
-            sa12 = self._create_array(text_new12)
+        if code < length02:
+            sa12 = self._create_array(text12)
 
             for i, suf in enumerate(sa12):
-                text_new12[suf] = i + 1
+                text12[suf] = i + 1
         else:
-            sa12 = [0] * lengths02
+            sa12 = [0] * length02
 
-            for i, ltr in enumerate(text_new12):
-                sa12[ltr - 1] = i
+            for i, code in enumerate(text12):
+                sa12[code - 1] = i
 
         sa0 = [3 * i for i in sa12 if i < lengths[2]]
-        SuffixArray._sort_by_keys(sa0, text, 0)
-        return self._merge(text, sa0, text_new12, sa12)
+        SuffixArray._sort_indices(sa0, txt, 0)
+        return self._merge(txt, sa0, text12, sa12)
 
     def _merge(self, text0, sa0, text12, sa12):
         # Merges suffix arrays for two texts:
@@ -138,59 +142,59 @@ class SuffixArray:
         # - text of thirds from indices giving 1 or 2 modulo 3
         sa_merged = []
         lengths = (len(text0) // 3, (len(text0) + 1) // 3, (len(text0) + 2) // 3)
-        ix0 = 0
-        ix12 = lengths[2] - lengths[1]
+        index0 = 0
+        index12 = lengths[2] - lengths[1]
 
-        while ix0 < len(sa0) and ix12 < len(sa12):
-            pos12 = sa12[ix12] * 3 + 1 if sa12[ix12] < lengths[2] else \
-                (sa12[ix12] - lengths[2]) * 3 + 2
-            pos0 = sa0[ix0]
+        while index0 < len(sa0) and index12 < len(sa12):
+            pos12 = sa12[index12] * 3 + 1 if sa12[index12] < lengths[2] else \
+                (sa12[index12] - lengths[2]) * 3 + 2
+            pos0 = sa0[index0]
 
-            if sa12[ix12] < lengths[2]:
-                cond = (self._get(text0, pos12), self._get(text12, sa12[ix12] + lengths[2])) \
+            if sa12[index12] < lengths[2]:
+                cond = (self._get(text0, pos12), self._get(text12, sa12[index12] + lengths[2])) \
                        <= (self._get(text0, pos0), self._get(text12, pos0 // 3))
             else:
                 cond = (self._get(text0, pos12), self._get(text0, pos12 + 1),
-                        self._get(text12, sa12[ix12] - lengths[2] + 1)) \
+                        self._get(text12, sa12[index12] - lengths[2] + 1)) \
                        <= (self._get(text0, pos0), self._get(text0, pos0 + 1),
                            self._get(text12, pos0 // 3 + lengths[2]))
 
             if cond:
                 sa_merged.append(pos12)
-                ix12 += 1
+                index12 += 1
             else:
                 sa_merged.append(pos0)
-                ix0 += 1
+                index0 += 1
 
-        while ix12 < len(sa12):
-            if sa12[ix12] < lengths[2]:
-                sa_merged.append(sa12[ix12] * 3 + 1)
+        while index12 < len(sa12):
+            if sa12[index12] < lengths[2]:
+                sa_merged.append(sa12[index12] * 3 + 1)
             else:
-                sa_merged.append((sa12[ix12] - lengths[2]) * 3 + 2)
+                sa_merged.append((sa12[index12] - lengths[2]) * 3 + 2)
 
-            ix12 += 1
+            index12 += 1
 
-        while ix0 < len(sa0):
-            sa_merged.append(sa0[ix0])
-            ix0 += 1
+        while index0 < len(sa0):
+            sa_merged.append(sa0[index0])
+            index0 += 1
 
         return sa_merged
 
     @staticmethod
-    def _sort_by_keys(indices, keys, shift):
+    def _sort_indices(indices, values, shift):
         # Sorts specified array of shifted indices of specified keys from a sized alphabet.
         buckets = {}
         j = 0
 
         for i in indices:
-            k = SuffixArray._get(keys, i + shift)
+            v = SuffixArray._get(values, i + shift)
 
-            if k not in buckets:
-                buckets[k] = deque()
+            if v not in buckets:
+                buckets[v] = deque()
 
-            buckets[k].append(i)
+            buckets[v].append(i)
 
-        for bucket in map(lambda p: p[1], sorted(buckets.items(), key=lambda p: p[0])):
+        for _, bucket in sorted(buckets.items(), key=lambda p: p[0]):
             while len(bucket) > 0:
                 indices[j] = bucket.popleft()
                 j += 1
