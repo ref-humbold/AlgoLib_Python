7fa56339847c30b00e4a9f6aa70e45c510b96625
R. Kaleta <rafkaleta@onet.pl>
2021-08-21 18:15:43 +0200

    Test sorted_by_angle when equal angles

--- tests/geometry/dim2/test_geometry_2d.py
+++ tests/geometry/dim2/test_geometry_2d.py
@@ -50,6 +50,18 @@ class Geometry2DTest(unittest.TestCase):
                               Point2D(-3, 2), Point2D(-3, -2), Point2D(-2, -3), Point2D(2, -3),
                               Point2D(3, -2)], result)
 
+    def test__sorted_by_angle__when_equal_angles__then_compare_radius(self):
+        # given
+        sequence = [Point2D(0.0, 0.0), Point2D(1.0, 1.0), Point2D(-2.0, -2.0), Point2D(-3.0, -3.0),
+                    Point2D(4.0, 4.0)]
+        # when
+        result = sorted_by_angle(sequence)
+        # then
+        self.assertIsInstance(result, list)
+        self.assertIsNot(sequence, result)
+        self.assertListEqual([Point2D(0.0, 0.0), Point2D(1.0, 1.0), Point2D(4.0, 4.0),
+                              Point2D(-2.0, -2.0), Point2D(-3.0, -3.0)], result)
+
     def test__distance__when_different_points__then_distance(self):
         # when
         result = distance(Point2D(4.0, 5.0), Point2D(-2.0, -3.0))
