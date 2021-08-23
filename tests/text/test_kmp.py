2ed99da2aed9b078f22611ec0e8b6fe87de64e6e
R. Kaleta <rafkaleta@onet.pl>
2021-08-23 19:10:57 +0200

    Using assertpy in tests for some text algorithms

--- tests/text/test_kmp.py
+++ tests/text/test_kmp.py
@@ -2,54 +2,50 @@
 """Tests: Knuth-Morris-Pratt algorithm"""
 import unittest
 
+from assertpy import assert_that
+
 from algolib.text import kmp
 
 
 class KMPTest(unittest.TestCase):
-    def test__kmp__when_pattern_found_once__then_single_occurrence(self):
-        text = "abcde"
-        pattern = "a"
+    @staticmethod
+    def test__kmp__when_pattern_found_once__then_single_occurrence():
+        # when
+        result = kmp("abcde", "a")
+        # then
+        assert_that(list(result)).is_equal_to([0])
 
-        result = kmp(text, pattern)
+    @staticmethod
+    def test__kmp__when_pattern_found_twice__then_two_occurrences():
+        # when
+        result = kmp("abcdae", "a")
+        # then
+        assert_that(list(result)).is_equal_to([0, 4])
 
-        self.assertListEqual([0], list(result))
+    @staticmethod
+    def test__kmp__when_pattern_found_twice_and_intersects__then_two_occurrences():
+        # when
+        result = kmp("aaabcde", "aa")
+        # then
+        assert_that(list(result)).is_equal_to([0, 1])
 
-    def test__kmp__when_pattern_found_twice__then_two_occurrences(self):
-        text = "abcdae"
-        pattern = "a"
+    @staticmethod
+    def test__kmp__when_pattern_not_found__then_empty_occurrences():
+        # when
+        result = kmp("abcde", "x")
+        # then
+        assert_that(list(result)).is_empty()
 
-        result = kmp(text, pattern)
+    @staticmethod
+    def test__kmp__when_pattern_is_empty_string__then_empty_occurrences():
+        # when
+        result = kmp("abcde", "")
+        # then
+        assert_that(list(result)).is_empty()
 
-        self.assertListEqual([0, 4], list(result))
-
-    def test__kmp__when_pattern_found_twice_and_intersects__then_two_occurrences(self):
-        text = "aaabcde"
-        pattern = "aa"
-
-        result = kmp(text, pattern)
-
-        self.assertListEqual([0, 1], list(result))
-
-    def test__kmp__when_pattern_not_found__then_empty_occurrences(self):
-        text = "abcde"
-        pattern = "x"
-
-        result = kmp(text, pattern)
-
-        self.assertListEqual([], list(result))
-
-    def test__kmp__when_pattern_is_empty_string__then_empty_occurrences(self):
-        text = "abcde"
-        pattern = ""
-
-        result = kmp(text, pattern)
-
-        self.assertListEqual([], list(result))
-
-    def test__kmp__when_text_is_empty_string__then_empty_occurrences(self):
-        text = ""
-        pattern = "a"
-
-        result = kmp(text, pattern)
-
-        self.assertListEqual([], list(result))
+    @staticmethod
+    def test__kmp__when_text_is_empty_string__then_empty_occurrences():
+        # when
+        result = kmp("", "a")
+        # then
+        assert_that(list(result)).is_empty()
--- tests/text/test_suffix_array.py
+++ tests/text/test_suffix_array.py
@@ -2,6 +2,8 @@
 """Tests: Structure of suffix array"""
 import unittest
 
+from assertpy import assert_that
+
 from algolib.text import SuffixArray
 
 
@@ -14,73 +16,89 @@ class SuffixArrayTest(unittest.TestCase):
         self.test_object = SuffixArray("mississippi")
 
     def test__len(self):
+        # when
         result = len(self.test_object)
-
-        self.assertEqual(11, result)
+        # then
+        assert_that(result).is_equal_to(11)
 
     def test__getitem(self):
+        # when
         result0 = self.test_object[0]
         result1 = self.test_object[3]
         result2 = self.test_object[6]
         result3 = self.test_object[9]
-
-        self.assertEqual("i", result0)
-        self.assertEqual("ississippi", result1)
-        self.assertEqual("ppi", result2)
-        self.assertEqual("ssippi", result3)
+        # then
+        assert_that(result0).is_equal_to("i")
+        assert_that(result1).is_equal_to("ississippi")
+        assert_that(result2).is_equal_to("ppi")
+        assert_that(result3).is_equal_to("ssippi")
 
     def test__getitem__when_out_of_range(self):
-        with self.assertRaises(IndexError):
-            _ = self.test_object[20]
+        # when
+        def function(i):
+            return self.test_object[i]
+
+        # then
+        assert_that(function).raises(IndexError).when_called_with(20)
 
     def test__index_at(self):
+        # when
         result0 = self.test_object.index_at(0)
         result1 = self.test_object.index_at(3)
         result2 = self.test_object.index_at(6)
         result3 = self.test_object.index_at(9)
-
-        self.assertEqual(10, result0)
-        self.assertEqual(1, result1)
-        self.assertEqual(8, result2)
-        self.assertEqual(5, result3)
+        # then
+        assert_that(result0).is_equal_to(10)
+        assert_that(result1).is_equal_to(1)
+        assert_that(result2).is_equal_to(8)
+        assert_that(result3).is_equal_to(5)
 
     def test__index_at__when_out_of_range(self):
-        with self.assertRaises(IndexError):
-            self.test_object.index_at(20)
+        # when
+        function = self.test_object.index_at
+        # then
+        assert_that(function).raises(IndexError).when_called_with(20)
 
     def test__index_of(self):
+        # when
         result0 = self.test_object.index_of(0)
         result1 = self.test_object.index_of(3)
         result2 = self.test_object.index_of(6)
         result3 = self.test_object.index_of(9)
-
-        self.assertEqual(4, result0)
-        self.assertEqual(8, result1)
-        self.assertEqual(7, result2)
-        self.assertEqual(5, result3)
+        # then
+        assert_that(result0).is_equal_to(4)
+        assert_that(result1).is_equal_to(8)
+        assert_that(result2).is_equal_to(7)
+        assert_that(result3).is_equal_to(5)
 
     def test__index_of__when_out_of_range(self):
-        with self.assertRaises(IndexError):
-            self.test_object.index_of(20)
+        # when
+        function = self.test_object.index_of
+        # then
+        assert_that(function).raises(IndexError).when_called_with(20)
 
     def test__lcp__when_same_suffix(self):
+        # when
         result = self.test_object.lcp(4, 4)
-
-        self.assertEqual(7, result)
+        # then
+        assert_that(result).is_equal_to(7)
 
     def test__lcp__when_different_suffix1(self):
+        # when
         result = self.test_object.lcp(1, 10)
-
-        self.assertEqual(1, result)
+        # then
+        assert_that(result).is_equal_to(1)
 
     def test__lcp__when_different_suffix2(self):
+        # when
         result = self.test_object.lcp(9, 6)
-
-        self.assertEqual(0, result)
+        # then
+        assert_that(result).is_equal_to(0)
 
     def test__lcp__when_swap_suffix(self):
+        # when
         result0 = self.test_object.lcp(2, 5)
         result1 = self.test_object.lcp(5, 2)
-
-        self.assertEqual(3, result0)
-        self.assertEqual(result0, result1)
+        # then
+        assert_that(result0).is_equal_to(3)
+        assert_that(result1).is_equal_to(result0)
