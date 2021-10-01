a281c81c0478fd3cc9818ba7210f674b91556afe
R. Kaleta <rafkaleta@onet.pl>
2021-10-02 00:40:27 +0200

    Assertpy in geometry

--- tests/geometry/dim2/test_closets_points.py
+++ tests/geometry/dim2/test_closets_points.py
@@ -2,6 +2,8 @@
 """Tests: Algorithm for pair of closest points in 2D"""
 import unittest
 
+from assertpy import assert_that
+
 from algolib.geometry.dim2 import Point2D, find_closest_points
 
 
@@ -9,28 +11,32 @@ class ClosestPointsTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def test__find_closest_points__when_one_point__then_this_point(self):
+    @staticmethod
+    def test__find_closest_points__when_one_point__then_this_point():
         # when
         result = find_closest_points([Point2D(2, 2)])
         # then
-        self.assertTupleEqual((Point2D(2, 2), Point2D(2, 2)), result)
+        assert_that(result).is_equal_to((Point2D(2, 2), Point2D(2, 2)))
 
-    def test__find_closest_points__when_two_points__then_these_points(self):
+    @staticmethod
+    def test__find_closest_points__when_two_points__then_these_points():
         # when
         result = find_closest_points([Point2D(2, 2), Point2D(4, 4)])
         # then
-        self.assertTupleEqual((Point2D(2, 2), Point2D(4, 4)), result)
+        assert_that(result).is_equal_to((Point2D(2, 2), Point2D(4, 4)))
 
-    def test__find_closest_points__when_three_points__then_pair_of_closest_points(self):
+    @staticmethod
+    def test__find_closest_points__when_three_points__then_pair_of_closest_points():
         # when
         result = find_closest_points([Point2D(3, 2), Point2D(1, 1), Point2D(7, 0)])
         # then
-        self.assertTupleEqual((Point2D(1, 1), Point2D(3, 2)), result)
+        assert_that(result).is_equal_to((Point2D(1, 1), Point2D(3, 2)))
 
-    def test__find_closest_points__when_multiple_points__then_pair_of_closest_points(self):
+    @staticmethod
+    def test__find_closest_points__when_multiple_points__then_pair_of_closest_points():
         # when
         result = find_closest_points(
             [Point2D(1, 1), Point2D(-2, 2), Point2D(-4, 4), Point2D(3, -3), Point2D(0, -5),
              Point2D(1, 0), Point2D(-7, 2), Point2D(4, 5)])
         # then
-        self.assertTupleEqual((Point2D(1, 1), Point2D(1, 0)), result)
+        assert_that(result).is_equal_to((Point2D(1, 1), Point2D(1, 0)))
--- tests/geometry/dim3/test_geometry_3d.py
+++ tests/geometry/dim3/test_geometry_3d.py
@@ -2,6 +2,8 @@
 """Tests: Algorithms for basic geometrical computations in 3D"""
 import unittest
 
+from assertpy import assert_that
+
 from algolib.geometry.dim3 import Point3D, Vector3D, distance, sorted_by_x, sorted_by_y, \
     sorted_by_z, translate
 
@@ -10,7 +12,8 @@ class Geometry3DTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def test__sorted_by_x__when_tuple__then_new_list_sorted_stably_ascending(self):
+    @staticmethod
+    def test__sorted_by_x__when_tuple__then_new_list_sorted_stably_ascending():
         # given
         sequence = (Point3D(0.0, 0.0, 0.0), Point3D(2.0, 3.0, -5.0), Point3D(-2.0, -3.0, 5.0),
                     Point3D(2.0, -3.0, -5.0), Point3D(-2.0, -3.0, -5.0), Point3D(3.0, 2.0, 5.0),
@@ -18,13 +21,14 @@ class Geometry3DTest(unittest.TestCase):
         # when
         result = sorted_by_x(sequence)
         # then
-        self.assertIsInstance(result, list)
-        self.assertListEqual([Point3D(-3.0, 2.0, 5.0), Point3D(-2.0, -3.0, 5.0),
-                              Point3D(-2.0, -3.0, -5.0), Point3D(0.0, 0.0, 0.0),
-                              Point3D(2.0, 3.0, -5.0), Point3D(2.0, -3.0, -5.0),
-                              Point3D(3.0, 2.0, 5.0)], result)
+        assert_that(result).is_instance_of(list)
+        assert_that(result).is_equal_to([Point3D(-3.0, 2.0, 5.0), Point3D(-2.0, -3.0, 5.0),
+                                         Point3D(-2.0, -3.0, -5.0), Point3D(0.0, 0.0, 0.0),
+                                         Point3D(2.0, 3.0, -5.0), Point3D(2.0, -3.0, -5.0),
+                                         Point3D(3.0, 2.0, 5.0)])
 
-    def test__sorted_by_y__when_list__then_new_list_sorted_stably_ascending(self):
+    @staticmethod
+    def test__sorted_by_y__when_list__then_new_list_sorted_stably_ascending():
         # given
         sequence = [Point3D(0.0, 0.0, 0.0), Point3D(2.0, 3.0, -5.0), Point3D(-2.0, -3.0, 5.0),
                     Point3D(2.0, -3.0, -5.0), Point3D(-2.0, -3.0, -5.0), Point3D(3.0, 2.0, 5.0),
@@ -32,14 +36,14 @@ class Geometry3DTest(unittest.TestCase):
         # when
         result = sorted_by_y(sequence)
         # then
-        self.assertIsInstance(result, list)
-        self.assertIsNot(sequence, result)
-        self.assertListEqual([Point3D(-2.0, -3.0, 5.0), Point3D(2.0, -3.0, -5.0),
-                              Point3D(-2.0, -3.0, -5.0), Point3D(0.0, 0.0, 0.0),
-                              Point3D(3.0, 2.0, 5.0), Point3D(-3.0, 2.0, 5.0),
-                              Point3D(2.0, 3.0, -5.0)], result)
+        assert_that(result).is_instance_of(list)
+        assert_that(result).is_equal_to([Point3D(-2.0, -3.0, 5.0), Point3D(2.0, -3.0, -5.0),
+                                         Point3D(-2.0, -3.0, -5.0), Point3D(0.0, 0.0, 0.0),
+                                         Point3D(3.0, 2.0, 5.0), Point3D(-3.0, 2.0, 5.0),
+                                         Point3D(2.0, 3.0, -5.0)])
 
-    def test__sorted_by_z__when_list__then_new_list_sorted_stably_ascending(self):
+    @staticmethod
+    def test__sorted_by_z__when_list__then_new_list_sorted_stably_ascending():
         # given
         sequence = [Point3D(0.0, 0.0, 0.0), Point3D(2.0, 3.0, -5.0), Point3D(-2.0, -3.0, 5.0),
                     Point3D(2.0, -3.0, -5.0), Point3D(-2.0, -3.0, -5.0), Point3D(3.0, 2.0, 5.0),
@@ -47,37 +51,40 @@ class Geometry3DTest(unittest.TestCase):
         # when
         result = sorted_by_z(sequence)
         # then
-        self.assertIsInstance(result, list)
-        self.assertIsNot(sequence, result)
-        self.assertListEqual([Point3D(2.0, 3.0, -5.0), Point3D(2.0, -3.0, -5.0),
-                              Point3D(-2.0, -3.0, -5.0), Point3D(0.0, 0.0, 0.0),
-                              Point3D(-2.0, -3.0, 5.0), Point3D(3.0, 2.0, 5.0),
-                              Point3D(-3.0, 2.0, 5.0)], result)
+        assert_that(result).is_instance_of(list)
+        assert_that(result).is_equal_to([Point3D(2.0, 3.0, -5.0), Point3D(2.0, -3.0, -5.0),
+                                         Point3D(-2.0, -3.0, -5.0), Point3D(0.0, 0.0, 0.0),
+                                         Point3D(-2.0, -3.0, 5.0), Point3D(3.0, 2.0, 5.0),
+                                         Point3D(-3.0, 2.0, 5.0)])
 
-    def test__distance__when_different_points__then_distance(self):
+    @staticmethod
+    def test__distance__when_different_points__then_distance():
         # when
         result = distance(Point3D(4.0, 8.0, 5.0), Point3D(-2.0, -1.0, 3.0))
         # then
-        self.assertEqual(11.0, result)
+        assert_that(result).is_equal_to(11.0)
 
-    def test__distance__when_same_point__then_zero(self):
+    @staticmethod
+    def test__distance__when_same_point__then_zero():
         # given
         point = Point3D(13.5, 6.5, -4.2)
         # when
         result = distance(point, point)
         # then
-        self.assertEqual(0.0, result)
+        assert_that(result).is_equal_to(0.0)
 
-    def test__translate__then_point_translated(self):
+    @staticmethod
+    def test__translate__then_point_translated():
         # when
         result = translate(Point3D(13.7, 6.5, -4.2), Vector3D(-10.4, 3.3, 1.1))
         # then
-        self.assertEqual(Point3D(3.3, 9.8, -3.1), result)
+        assert_that(result).is_equal_to(Point3D(3.3, 9.8, -3.1))
 
-    def test__translate__when_zero_vector__then_same_point(self):
+    @staticmethod
+    def test__translate__when_zero_vector__then_same_point():
         # given
         point = Point3D(13.5, 6.5, -4.2)
         # when
         result = translate(point, Vector3D(0.0, 0.0, 0.0))
         # then
-        self.assertEqual(point, result)
+        assert_that(result).is_equal_to(point)
