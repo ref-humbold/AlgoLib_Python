9234d732c61d3f2c152ac0ff4094c808e73f04ca
R. Kaleta <rafkaleta@onet.pl>
2021-12-28 00:29:19 +0100

    LCS with single list

--- algolib/sequences/longest_common_subsequence.py
+++ algolib/sequences/longest_common_subsequence.py
@@ -8,15 +8,15 @@ _T = TypeVar("_T")
 def count_lcs_length(sequence1: Sequence[_T], sequence2: Sequence[_T]):
     short_seq, long_seq = \
         (sequence1, sequence2) if len(sequence1) <= len(sequence2) else (sequence2, sequence1)
-    prev_lcs = [0] * (len(short_seq) + 1)
+    lcs = [0] * (len(short_seq) + 1)
 
     for element1 in long_seq:
-        next_lcs = [0]
+        previous_above = lcs[0]
 
         for i, element2 in enumerate(short_seq):
-            next_lcs.append(
-                prev_lcs[i] + 1 if element1 == element2 else max(next_lcs[-1], prev_lcs[i + 1]))
+            previous_diagonal = previous_above
+            previous_above = lcs[i + 1]
+            lcs[i + 1] = \
+                previous_diagonal + 1 if element1 == element2 else max(previous_above, lcs[i])
 
-        prev_lcs = next_lcs
-
-    return prev_lcs[-1]
+    return lcs[-1]
--- tests/sequences/test_longest_common_subsequence.py
+++ tests/sequences/test_longest_common_subsequence.py
@@ -9,11 +9,21 @@ from algolib.sequences import count_lcs_length
 
 class LongestCommonSubsequenceTests(unittest.TestCase):
     @staticmethod
-    def test__count_lcs_length__when_empty_sequence__then_zero():
+    def test__count_lcs_length__when_empty__then_zero():
         # when
         result = count_lcs_length("qwertyuiop", "")
         # then
         assert_that(result).is_equal_to(0)
+
+    @staticmethod
+    def test__count_lcs_length__when_same_character_text__then_shorter_length():
+        # given
+        size = 4
+        text = "x" * size
+        # when
+        result = count_lcs_length(text + text, text)
+        # then
+        assert_that(result).is_equal_to(size)
 
     @staticmethod
     def test__count_lcs_length__when_repeated_single_element__then_one():
@@ -23,16 +33,16 @@ class LongestCommonSubsequenceTests(unittest.TestCase):
         assert_that(result).is_equal_to(1)
 
     @staticmethod
-    def test__count_lcs_length__when_same_sequence__then_sequence_length():
+    def test__count_lcs_length__when_same_text__then_text_length():
         # given
-        sequence = "qwertyuiop"
+        text = "qwertyuiop"
         # when
-        result = count_lcs_length(sequence, sequence)
+        result = count_lcs_length(text, text)
         # then
-        assert_that(result).is_equal_to(len(sequence))
+        assert_that(result).is_equal_to(len(text))
 
     @staticmethod
-    def test__count_lcs_length__when_subsequence__then_subsequence_length():
+    def test__count_lcs_length__when_subtext__then_subtext_length():
         # when
         result = count_lcs_length("qwertyuiop", "zxqwertyasdfuiopcvb")
         # then
@@ -46,8 +56,34 @@ class LongestCommonSubsequenceTests(unittest.TestCase):
         assert_that(result).is_equal_to(0)
 
     @staticmethod
-    def test__count_lcs_length__when_common_subsequence__then_common_subsequence_length():
+    def test__count_lcs_length__when_common_subtext__then_common_subtext_length():
         # when
         result = count_lcs_length("qwertyuiop", "zxrtyasdfuiopcvb")
+        # then
+        assert_that(result).is_equal_to(len("rtyuiop"))
+
+    @staticmethod
+    def test__count_lcs_length__when_same_element_sequence__then_shorter_length():
+        # given
+        size = 25
+        sequence = [11] * size
+        # when
+        result = count_lcs_length(sequence, sequence + sequence)
+        # then
+        assert_that(result).is_equal_to(size)
+
+    @staticmethod
+    def test__count_lcs_length__when_same_sequence__then_sequence_length():
+        # given
+        sequence = list(map(ord, "qwertyuiop"))
+        # when
+        result = count_lcs_length(sequence, sequence)
+        # then
+        assert_that(result).is_equal_to(len(sequence))
+
+    @staticmethod
+    def test__count_lcs_length__when_common_subsequence__then_common_subsequence_length():
+        # when
+        result = count_lcs_length(list(map(ord, "qwertyuiop")), list(map(ord, "zxrtyasdfuiopcvb")))
         # then
         assert_that(result).is_equal_to(len("rtyuiop"))
